112483fd160641991883f7cd47e70a386b6e47c1
Chris Arndt <carndt@cals.wisc.edu>
2015-05-27 08:07:05 -0500

    a) Find paths to csv files. b) Pass those paths to the Curriculum-Maker class.

--- curriculem_maker.rb
+++ curriculem_maker.rb
@@ -3,13 +3,25 @@ require 'csv'
 
 class CurriculumMaker
 
-  attr_reader :course_data, # [array]
+  attr_reader :course_data, # [array] of hashes.
               :template
 
   def initialize
     @course_data = []
     @template = 'Template missing'
   end
+
+
+  # Add CSV contents
+  # a) ".shift" is a method that takes the first item off an array, and destructively removes it, and
+  #    passes it in to wherever we assign it.
+  #
+  def add_course_data(filepath)
+    courses = CSV.read(filepath)
+    header = courses.shift
+    @course_data << { :header => header, :courses => courses }
+  end
+
 
   # Add a template
   # a) Using private method "binding". So course data will be available b/c it is included in "binding".
--- merge_template.rb
+++ merge_template.rb
@@ -9,11 +9,18 @@ site_dir = File.join(this_dir, 'site')
 assets_dir = File.join(site_dir, '_assets')
 target_dir = File.join(site_dir, 'programs')
 
+# define data and template files
+semester_files = ['first_semester.csv', 'second_semester.csv']
+
 # create a new CurriculumMaker
 cm = CurriculumMaker.new
 
 # add the template
 # add the course data
+semester_files.each do |semester_file|
+  semester_path = File.join(assets_dir, semester_file)
+  cm.add_course_data(semester_path)
+end
 
 # render output and capture in a variable
 content = cm.render
